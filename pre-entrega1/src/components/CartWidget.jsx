f9252e0f827932f71165f088f4d1acec50a3bcae
Guillermo Grisi <gfg1977@gmail.com>
2024-11-08 08:11:40 -0300

    cambie forma del logo

--- pre-entrega1/src/components/CartWidget.jsx
+++ pre-entrega1/src/components/CartWidget.jsx
@@ -3,7 +3,7 @@ import { TiShoppingCart } from "react-icons/ti";
 function CartWidget() {
   return (
     <div className="carrito">
-        <TiShoppingCart size="50px" />
+        <TiShoppingCart size="50px"/>
         <span className="badge">3</span>
     </div>
   )
